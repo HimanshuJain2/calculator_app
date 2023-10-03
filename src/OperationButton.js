0ccbc244f85707b6b8db6ff1ceb2222a711c77c6
HimanshuJain2 <mail2jainhimanshu@gmail.com>
2023-10-03 23:20:25 +0530

    update-react-app

---
+++ src/OperationButton.js
@@ -0,0 +1,7 @@
+import { ACTIONS } from "./Calculator";
+export default function OperationButton({dispatch,operation}){
+    return <button onClick={()=>dispatch({
+        type:ACTIONS.CHOOSE_OPERATION, payload: {operation}
+    })}>{operation}
+    </button>
+}
